5f788b8e8b8e3d4c793ce90269ea6f9935704e7a
chris-paul <18270890760@163.com>
2021-03-25 21:54:43 +0800

    heap

---
+++ src/algorithm/heap/index.tsx
@@ -0,0 +1,104 @@
+/* eslint-disable class-methods-use-this */
+/*
+ * @Author: 廉恒凯
+ * @Date: 2021-03-24 20:20:15
+ * @LastEditors: 廉恒凯
+ * @LastEditTime: 2021-03-24 21:08:10
+ * @Description: file content
+ */
+class MinHeap<T> {
+  protected heap: T[];
+
+  constructor() {
+    this.heap = [];
+  }
+
+  // 获取左子节点的位置
+  protected getLeftIndex(index: number): number {
+    return 2 * index + 1;
+  }
+
+  // 获取右子节点的位置
+  protected getRightIndex(index: number): number {
+    return 2 * index + 2;
+  }
+
+  // 获取父节点的位置
+  protected getParentIndex(index: number): number | undefined {
+    if (index === 0) {
+      return undefined;
+    }
+    return Math.floor((index - 1) / 2);
+  }
+
+  protected swap(parentIndex: number, index: number): void {
+    [this.heap[index], this.heap[parentIndex]] = [
+      this.heap[parentIndex],
+      this.heap[index],
+    ];
+  }
+
+  protected siftUp(index: number): void {
+    // 获取父节点的位置
+    const parentIndex = this.getParentIndex(index);
+    if (
+      parentIndex !== undefined &&
+      this.heap[parentIndex] > this.heap[index]
+    ) {
+      this.swap(parentIndex, index);
+      this.siftUp(parentIndex);
+    }
+  }
+
+  protected siftDown(index: number): void {
+    // 获取父节点的位置
+    const leftIndex = this.getLeftIndex(index);
+    const rightIndex = this.getLeftIndex(index);
+    if (leftIndex !== undefined && this.heap[leftIndex] < this.heap[index]) {
+      this.swap(leftIndex, index);
+      this.siftDown(leftIndex);
+    }
+
+    if (rightIndex !== undefined && this.heap[rightIndex] < this.heap[index]) {
+      this.swap(rightIndex, index);
+      this.siftDown(rightIndex);
+    }
+  }
+
+  /**
+   *先将元素放入堆中
+   * 堆中的元素一直向前移动 直到父节点小于等于这个插入的值
+   * 时间复杂度 O(lgn) n为节点的数目
+   * @param value
+   * @returns
+   */
+  insert(value: T): boolean {
+    if (value != null) {
+      this.heap.push(value);
+      this.siftUp(this.heap.length - 1);
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * 删除堆顶元素 先用数组尾部元素替换堆顶元素,然后将
+   * 新的堆顶向下移动直到子节点大于等于这个元素
+   * 时间复杂度 O(lgn) n为节点的数目
+   * @param value
+   */
+  pop(): void {
+    if (this.heap.length === 0) return;
+    const value = this.heap.pop();
+    if (value) {
+      this.heap[0] = value;
+      this.siftDown(0);
+    }
+  }
+
+  size(): number {
+    return this.heap.length;
+  }
+}
+
+export default MinHeap;
