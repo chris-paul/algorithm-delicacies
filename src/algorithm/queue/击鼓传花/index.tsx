1eda7c5ec9b5cc32fb9c7fe5d563b49cc7d69df8
chris-paul <18270890760@163.com>
2022-05-25 20:31:38 +0800

    击鼓传花

--- src/algorithm/queue/击鼓传花/index.tsx
+++ src/algorithm/queue/击鼓传花/index.tsx
@@ -2,15 +2,12 @@
  * @Author: 廉恒凯
  * @Date: 2021-02-21 21:01:03
  * @LastEditors: 廉恒凯
- * @LastEditTime: 2021-02-22 21:29:00
+ * @LastEditTime: 2021-05-16 20:43:42
  * @Description: file content
  */
 import CircularQueue from '@dataStructure/queue/circularQueue/index';
 
-const passFlower = (
-  nameList: string[],
-  loopNum: number
-): string | null => {
+const passFlower = (nameList: string[], loopNum: number): string | null => {
   const nameLen = nameList.length;
   const queue = new CircularQueue<string>(nameLen);
   for (let i = 0; i < nameLen; i += 1) {
